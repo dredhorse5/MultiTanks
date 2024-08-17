8e8e5a7daaa5855bac355a7ec9775d05b3d8936d
dredhorse5 <Dima.titov18@gmail.com>
2024-08-17 18:27:35 +0300

    some

--- Assets/_MultiTanks/Scripts/Tank/TankBody.cs
+++ Assets/_MultiTanks/Scripts/Tank/TankBody.cs
@@ -17,21 +17,17 @@ namespace MultiTanks
         public AnimationCurve ForceCurveBySpeed= AnimationCurve.Constant(0,1,1);
         public float Torque = 2.5f;
         [Space] 
-        [Min(0)] public float SideFriction = 1;
-        [Min(0)] public float ForwardFriction = 1;
         public AnimationCurve TorqueForceBySpeed;
 
-        public Transform ForcePoint;
         public List<TankWheel> Wheels;
 
 
 
         public float wheelGroundedValue { get; protected set; }
-        public Vector3 forwardVelocity => Vector3.Project(Rigidbody.velocity, transform.forward);
-        public Vector3 sideVelocity => Vector3.Project(Rigidbody.velocity, transform.right);
         private Rigidbody Rigidbody => owner.Tank.Rigidbody;
 
         private TankBodyNet owner;
+        
         private float forceValue;
         private float torqueValue;
 
@@ -44,20 +40,17 @@ namespace MultiTanks
 
         private void FixedUpdate()
         {
-            CalculateWheelGrounded();
+            wheelGroundedValue =Wheels.Find(_ => _.isGround) ? 1 : 0;
 
+            Vector3 forwardVelocity = Vector3.Project(Rigidbody.velocity, transform.forward);
 
             float velocity = forwardVelocity.magnitude * (Vector3.Angle(forwardVelocity, transform.forward) > 90f ? 1f : -1f);
-            Debug.Log(velocity);
             var force = ForceCurveBySpeed.Evaluate(velocity > 0 ? Mathf.InverseLerp(0f, MaxSpeed, velocity) : Mathf.InverseLerp(0f,-MaxBackSpeed, velocity)) * MoveForce;
 
             Wheels.ForEach(_ => _.SetForce(-force * forceValue));
-            /*Rigidbody.AddForceAtPosition(-1000f * curForce * forceValue * transform.forward, ForcePoint.position);
-            Rigidbody.AddForce(1000f * -ForwardFriction * wheelGroundedValue * forwardVelocity);*/
-            if (wheelGroundedValue <= 0.01f)
-                return;
-            Rigidbody.AddTorque(1000f * Torque * torqueValue * transform.up * TorqueForceBySpeed.Evaluate(Rigidbody.velocity.magnitude));
-            Rigidbody.AddForce(1000f * -sideVelocity * SideFriction);
+            
+            if (wheelGroundedValue > 0.01f)
+                Rigidbody.AddTorque(1000f * Torque * torqueValue * transform.up * TorqueForceBySpeed.Evaluate(Rigidbody.velocity.magnitude));
         }
         
 
@@ -67,37 +60,6 @@ namespace MultiTanks
 
         public void SetTorque(float value)=> torqueValue = value;
 
-
-
-
-
-        private void CalculateWheelGrounded()
-        {
-            wheelGroundedValue =Wheels.Find(_ => _.currentValueDistance < 1f) ? 1 : 0;
-            /*float dist = 1;
-            foreach (var wh in Wheels)
-            {
-                if (wh.currentValueDistance < dist)
-                    dist = wh.currentValueDistance;
-            }*/
-
-            //wheelGroundedValue = (1f - dist); //Wheels.Find(_ => _.currentValueDistance < 1f) ? 1 : 0;
-            /*
-            wheelGroundedValue = 0;
-            foreach (var wheel in LeftWheels)
-            {
-                if (wheel.isGrounded)
-                    wheelGroundedValue++;
-            }
-
-            foreach (var wheel in RightWheels)
-            {
-                if (wheel.isGrounded)
-                    wheelGroundedValue++;
-            }
-
-            wheelGroundedValue /= LeftWheels.Length + RightWheels.Length;*/
-        }
 
         public enum Types : byte
         {
